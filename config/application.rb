78892e03879762cb63fbe3332df945b8840e2d00
Joao Goncalves <goncalves.joao@gmail.com>
2021-11-16 18:54:24 +0000

    views yaml translation files configuration

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module NotDevise
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
   end
 end
