d63712d8aab790d361b5f56d7383e559d69563cb
Joao Goncalves <goncalves.joao@gmail.com>
2021-11-16 20:50:19 +0000

    controller translations in place

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,7 +10,7 @@ module Users
       if @user_login_form.valid_credentials?
         session[:user_id] = @user_login_form.user.id.to_s
 
-        redirect_to root_path, notice: t('.notice')
+        redirect_to root_path, notice: t('.notice', username: @user_login_form.username)
       else
         # If the form's validations pass but the credentials didn't,
         # then it means the password doesn't match the username
